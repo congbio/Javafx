b358683019e604c5490c0d3ef3350c73cc3d531e
congbio <cong.a23@student.passerellesnumeriques.org>
2022-07-03 10:24:43 +0700

    update file Room create again getter setter

--- src/main/java/com/example/hotelmanagement/Room.java
+++ src/main/java/com/example/hotelmanagement/Room.java
@@ -4,27 +4,65 @@ public class Room {
     public int id;
     public String name;
     public float price;
-    public String description;
-//    public String image;
-//// contructor add image
-//    public Room(int id, String name, float price, String description, String image) {
-//        this.id = id;
-//        this.name = name;
-//        this.price = price;
-//        this.description = description;
-//        this.image = image;
-//    }
 
-    public Room(String name, float price, String description) {
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String description;
+    public String image;
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public void setPrice(float price) {
         this.price = price;
+    }
+
+    public void setDescription(String description) {
         this.description = description;
     }
 
-    public Room(int id, String name, float price, String description) {
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+
+
+
+    public Room(String name, float price, String description, String image) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.image = image;
+
+    }
+
+    public Room(int id, String name, float price, String description, String image) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.description = description;
+        this.image = image;
     }
 }
