0bbe43d03d305cf60641eee72ce782f54021239d
congbio <cong.a23@student.passerellesnumeriques.org>
2022-07-05 09:49:59 +0700

    upload file

--- src/main/java/com/example/hotelmanagement/HomePage.java
+++ src/main/java/com/example/hotelmanagement/HomePage.java
@@ -58,189 +58,7 @@ public class HomePage extends Application {
         return gridPane;
     }
 
-    private void addUIControls(GridPane gridPane, Stage stage) {
-        // Add Header
-        Label headerLabel = new Label("Registration Form");
-        headerLabel.setFont(Font.font("Arial", FontWeight.BOLD, 24));
-        gridPane.add(headerLabel, 0,0,2,1);
-        GridPane.setHalignment(headerLabel, HPos.CENTER);
-        GridPane.setMargin(headerLabel, new Insets(20, 0,20,0));
 
-        // Add Name Label
-        Label nameLabel = new Label("Full Name : ");
-        gridPane.add(nameLabel, 0,1);
-
-        // Add Name Text Field
-        TextField  nameuser = new TextField();
-        nameuser.setPrefHeight(40);
-        gridPane.add(nameuser, 1,1);
-
-
-        // Add Email Label
-        Label emailLabel = new Label("Email ID : ");
-        gridPane.add(emailLabel, 0, 2);
-
-        // Add Email Text Field
-        TextField  emailuser = new TextField();
-          emailuser.setPrefHeight(40);
-        gridPane.add(emailuser, 1, 2);
-
-        // Add Password Label
-        Label passwordLabel = new Label("Password : ");
-        gridPane.add(passwordLabel, 0, 3);
-
-        // Add Password Field
-        TextField passuser = new TextField();
-        passuser.setPrefHeight(40);
-
-        gridPane.add(passuser, 1, 3);
-
-        // Add Submit Button
-        Button submitButton = new Button("Submit");
-
-        submitButton.setPrefHeight(40);
-        submitButton.setDefaultButton(true);
-        submitButton.setPrefWidth(100);
-        gridPane.add(submitButton, 0, 4, 2, 1);
-        GridPane.setHalignment(submitButton, HPos.CENTER);
-        GridPane.setMargin(submitButton, new Insets(20, 0,20,0));
-
-        submitButton.setOnAction(actionEvent -> {
-            this.checkRegister(stage, gridPane,nameuser,passuser,emailuser);
-        });
-    }
-    private  void checkRegister(Stage stage,GridPane gridPane,TextField nameuser, TextField passuser, TextField emailuser){
-        DBConnect con = new DBConnect();
-        ArrayList<customers> ad = (ArrayList<customers>) con.getCustomers();
-
-        String inputName = nameuser.getText();
-        String inputpass = passuser.getText();
-        String inputemail = emailuser.getText();
-
-
-        if(inputName.isEmpty()) {
-            showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), "Form Error!", "Please enter your name");
-            return;
-        }
-        if(inputemail.isEmpty()) {
-            showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), "Form Error!", "Please enter your email id");
-            return;
-        }
-        if(inputpass.isEmpty()) {
-            showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), "Form Error!", "Please enter a password");
-            return;
-        }
-
-
-        for (int i = 0; i < ad.size(); i++) {
-            if( inputemail.equals(ad.get(i).email)){
-                showAlertErroRegister();
-                this.ClearFormRegister( nameuser,  emailuser,  passuser);
-                return;
-
-            }
-
-        }
-        con.insertCustomer(new customers(inputName,inputemail,inputpass));
-        RegisterSuccess();
-        stage.setScene(loginScence);
-    }
-    void showAlertErroRegister(){
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Registger");
-        alert.setHeaderText("Hi You " );
-        alert.setContentText("Tài khoản đã tồn  tại vui lòng đăng ky tài khoản khác");
-        alert.show();
-    }
-    private void RegisterSuccess(  ){
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Register");
-        alert.setContentText("Bạn đã Đăng ký thành công");
-        alert.setContentText("Mời bạn đăng nhập để xem phòng !");
-        alert.show();
-    }
-    private void ClearFormRegister(TextField inputName, TextField inputEmail, TextField inputPass){
-        inputEmail.clear();
-        inputPass.clear();
-        inputName.clear();
-
-    }
-
-    private void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
-        Alert alert = new Alert(alertType);
-        alert.setTitle(title);
-        alert.setHeaderText(null);
-        alert.setContentText(message);
-        alert.initOwner(owner);
-        alert.show();
-    }
-
-    Scene showLogin(Stage stage){
-        VBox loginPage = new VBox();
-        Label labelLogin =new Label("LOGIN ACCOUNT");
-        Label Aname = new Label("Name: ");
-        Label Apassword = new Label("Password: ");
-        TextField  nameuser = new TextField();
-        TextField passuser= new TextField();
-        HBox fieldName = new HBox();
-
-        fieldName.getChildren().addAll(Aname,nameuser);
-        fieldName.setSpacing(20);
-        fieldName.setAlignment(Pos.BASELINE_CENTER);
-        HBox fieldPass = new HBox();
-        fieldPass.getChildren().addAll(Apassword,passuser);
-        fieldPass.setSpacing(10);
-        fieldPass.setAlignment(Pos.BASELINE_CENTER);
-
-        Button btnGoBack = new Button("Register");
-        btnGoBack.setOnAction(actionEvent -> {
-            stage.setScene(screenregister);
-            stage.centerOnScreen();
-        });
-        Button btnLogin = new Button("LOGIN");
-        btnLogin.setOnAction(actionEvent -> {
-            this.checkLogin(stage,nameuser,passuser);
-        });
-        HBox btnLoginPage = new HBox();
-        btnLoginPage.getChildren().addAll(btnLogin, btnGoBack );
-        btnLoginPage.setSpacing(10);
-        btnLoginPage.setAlignment(Pos.BASELINE_CENTER);
-        loginPage.getChildren().addAll(labelLogin,fieldName,fieldPass,btnLoginPage);
-        loginPage.setSpacing(15);
-        loginPage.setAlignment(Pos.BASELINE_CENTER);
-
-        return new Scene(loginPage, 300,300);
-    }
-    private void checkLogin(Stage stage,TextField nameuser, TextField passuser){
-        DBConnect con = new DBConnect();
-        ArrayList<customers> ad = (ArrayList<customers>) con.getCustomers();
-        String inputName =  nameuser.getText();
-        String inputPass =  passuser.getText();
-        boolean check = false;
-        for (int i = 0; i < ad.size(); i++) {
-
-            if(inputName.equals(ad.get(i).cusName)&& inputPass.equals(ad.get(i).password)){
-                System.out.println("Than cong!! ");
-                LoginSuccess(inputName);
-                stage.setScene(homeScene);
-                stage.centerOnScreen();
-                check = true;
-                break;
-            }
-
-        }
-        if( !check ){
-            LoginError();
-        }
-
-    }
-    private void LoginSuccess(String inputName ){
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Login");
-        alert.setHeaderText("Hi "+inputName );
-        alert.setContentText("Login successfully!");
-        alert.show();
-    }
     private void LoginError() {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("ERROR");
@@ -373,7 +191,14 @@ public class HomePage extends Application {
             lbPrice.setPadding(new Insets(5));
             lbName.setPadding(new Insets(5));
 
-//            --
+            vbRoom.getChildren().addAll(imageView,lbName,lbPrice, btnView);
+            grid.add((vbRoom), xgrid, ygrid);
+
+            xgrid +=1;
+            if( xgrid == 3){
+                xgrid = 0;
+                ygrid +=1;
+            }
 
         }
     }
